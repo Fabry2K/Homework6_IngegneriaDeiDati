89a47111464101e32957d92821808180a2cb3657
silviatommaso <silvia.tommaso18112002@gmail.com>
2026-02-10 11:31:33 +0100

    dedupe_update

--- dedupe.py
+++ dedupe.py
@@ -15,3 +15,36 @@ def couples_to_python_dictionary(ground_truth):
 
         # aggiungiamo la coppia etichettata
         labeled_pairs.append((dataset_a, dataset_b, match))
+
+    return labeled_pairs
+
+
+def fields_to_dedupe_format():
+
+    fields = [
+        {'field': 'manufacturer', 'type': 'String'},
+        {'field': 'model', 'type': 'String'},
+        {'field': 'year', 'type': 'String'},
+        {'field': 'mileage', 'type': 'String'},
+        {'field': 'fuel_type', 'type': 'String'},
+        {'field': 'transmission', 'type': 'String'},
+        {'field': 'body_type', 'type': 'String'},
+        {'field': 'cylinders', 'type': 'String'},
+        {'field': 'drive', 'type': 'String'},
+        {'field': 'color', 'type': 'String'},
+    ]
+
+    linker = dedupe.RecordLink(fields)
+
+    return linker
+
+
+def dedupe_training(ground_truth):
+
+    labeled_pairs = couples_to_python_dictionary(ground_truth)
+    linker = fields_to_dedupe_format()
+
+    linker.markPairs(labeled_pairs)
+    linker.train()
+
+    return linker
